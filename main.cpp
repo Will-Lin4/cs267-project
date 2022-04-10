87c1c9c363e840b2e92f895b45108162fbfe4821
Rajat Sirohi <sirohirajat6@gmail.com>
2022-04-09 22:23:50 -0700

    generate vectors wrt sparsity and distribution

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <cstring>
 #include <iostream>
 #include <map>
+#include <random>
 #include <mpi.h>
 
 // =================
@@ -29,7 +30,17 @@ int find_int_arg(int argc, char** argv, const char* option, int default_value) {
     return default_value;
 }
 
-char* find_string_option(int argc, char** argv, const char* option, char* default_value) {
+float find_float_arg(int argc, char** argv, const char* option, float default_value) {
+    int iplace = find_arg_idx(argc, argv, option);
+
+    if (iplace >= 0 && iplace < argc - 1) {
+        return std::stof(argv[iplace + 1]);
+    }
+
+    return default_value;
+}
+
+char* find_string_arg(int argc, char** argv, const char* option, char* default_value) {
     int iplace = find_arg_idx(argc, argv, option);
 
     if (iplace >= 0 && iplace < argc - 1) {
@@ -37,6 +48,18 @@ char* find_string_option(int argc, char** argv, const char* option, char* defaul
     }
 
     return default_value;
+}
+
+float find_distribution_parameter(int argc, char** argv, const char* distribution) {
+	float dist_param = find_float_arg(argc, argv, "-p", -1);
+
+	if (!strcmp(distribution, "geometric")) {
+		if (dist_param < 0 || dist_param > 1) dist_param = 0.5;
+	} else if (!strcmp(distribution, "poisson")) {
+		if (dist_param < 0) dist_param = 4;
+	}
+
+	return dist_param;
 }
 
 // Initializes a vector with the values [rank + 1, rank + 1, ..., rank + 1]
@@ -71,6 +94,41 @@ bool test_dummy_reduced_vector(const int num_procs,
     return true;
 }
 
+void generate_vector(std::map<int, int>& vector, const int vector_len, const int num_procs, const int rank,
+					 const float sparsity, const char* distribution, const float dist_param) {
+	std::random_device rd;
+	std::mt19937 gen(rd());
+	std::uniform_int_distribution<> uniform(0, vector_len-1);
+	std::geometric_distribution<> geometric(dist_param);
+	std::poisson_distribution<> poisson(dist_param);
+
+	int nz_count = 0, nz_max = std::round(sparsity * vector_len);
+	while (nz_count < nz_max) {
+		int idx;
+		if (!strcmp(distribution, "uniform")) idx = uniform(gen);
+		else if (!strcmp(distribution, "geometric")) idx = geometric(gen);
+		else if (!strcmp(distribution, "poisson")) idx = poisson(gen);
+		else { std::cerr << "Distribution '" << distribution << "' not found\n"; break; }
+
+		if (!vector.count(idx)) {
+			vector.emplace(idx, 1);
+			nz_count++;
+		}
+    }
+}
+
+void display_sparse_vector(std::map<int, int>& vector, const int vector_len) {
+	std::cout << "[ ";
+    for (int i = 0; i < vector_len; i++) {
+		if (vector.count(i)) {
+			std::cout << vector[i] << ' ';
+		} else {
+			std::cout << "0 ";
+		}
+	}
+	std::cout << "]\n";
+}
+
 // ==============
 // Main Function
 // ==============
@@ -91,12 +149,16 @@ int main(int argc, char** argv) {
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
     int vector_len = find_int_arg(argc, argv, "-n", 1000);
+    float sparsity = find_float_arg(argc, argv, "-s", 0.3);
+    char* distribution = find_string_arg(argc, argv, "-d", (char*) "uniform");
+    float dist_param = find_distribution_parameter(argc, argv, distribution);
 
     std::map<int, int> in_vector;
     std::map<int, int> reduced_vector;
 
-    // TODO: Replace with an actual generator
-    init_dummy_vector(num_procs, rank, vector_len, in_vector);
+    // Generate input vector
+    generate_vector(in_vector, vector_len, num_procs, rank, sparsity, distribution, dist_param);
+    //display_sparse_vector(in_vector, vector_len);
     
     // Algorithm
     auto start_time = std::chrono::steady_clock::now();
